69381d3fbb6186062c71282a9cb0f2ad81a04fc6
Weslley Rodrigues <weslley.rdgs@gmail.com>
2022-11-04 18:22:32 -0300

    feat: apostila05 app lembrete secao 2.1

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import {useState} from 'react';
+
 
 export default function App() {
+
+  const [lembrete, setLembrete] = useState('')
+  const capturarLembrete = (lembreteDigitado) =>{
+    setLembrete(lembreteDigitado)
+  
+  }
+
+  const adicionarLembrete = () => {
+    console.log(lembrete)
+  }
+
+  //single source of truth
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <TextInput
+          placeholder='Lembrar...'
+          style={styles.lembreteTextInput}
+          //onChangeText={capturarLembrete}
+          onChange={(e)=> capturarLembrete(e.target.value)}
+          value={lembrete}
+        />
+        <Button
+          title="Adicionar"
+          onPress={adicionarLembrete}
+        />
+      </View>
     </View>
+
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 40
   },
+  lembreteTextInput:{
+    borderBottomColor: '#CCC', 
+    borderBottomWidth: 1, 
+    marginBottom: 4, 
+    padding: 12}
 });
